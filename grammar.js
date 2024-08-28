5144d337d413deda3fa50921e82dfbff12a228ec
satler-git <mitsclatw24@mbr.nifty.com>
2024-08-28 21:25:53 +0900

    Add imm statement

--- grammar.js
+++ grammar.js
@@ -18,7 +18,39 @@ module.exports = grammar({
     _statement: $ => choice(
       // TODO:
       $.quit_statement,
+      $.imm_statement
     ),
+
     quit_statement: $ => 'quit',
+
+    ptr: $ => seq(/\$(m|n|lm|ln)/, choice(
+        seq(/\d+/, optional(/v\d+/)),
+        seq(
+          '[',
+          /\d+/,
+          seq(',', /\d+/),
+          seq(',', /\d+/),
+          seq(',', /\d+/),
+          ']'
+        ),
+      ),
+    ),
+
+    natural_num: $ => $._num,
+
+    _num: $ => choice(
+      /\d+/,
+      /0b[0-1]+/,
+      /0o[0-7]+/,
+      /0x[0-9A-Fa-f]+/,
+    ),
+
+    immudate_num: $ => choice(
+      seq(/(f|h)"/, optional(/-|\+/), /\d+.\d+/, /"/),
+      seq(/(i|s)"/, optional(/-|\+/), $._num, /"/),
+      seq(/(ui|us)"/, $._num, /"/),
+    ),
+
+    imm_statement: $ => seq('imm', $.immudate_num, $.ptr)
   }
 })
